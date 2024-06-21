699ca00043a11b24baec667132acf7168aecb1fd
Arjun P <arjunp946@gmail.com>
2024-06-21 19:10:33 +0530

    alphabets cross also made

--- patterns/crossalfebatic.c
+++ patterns/crossalfebatic.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int i,j,limit,k=0;
+    int i,j,limit;
+    char k='A'-1;
     printf("enter the limit: ");
     scanf("%d",&limit);
     for(i=1;i<=limit*2-1;i++){
         i<=limit?k++:k--;
         for(j=1;j<=limit*2-1;j++){
             if(j==i||j==limit*2-i){
-                printf("%d",k);
+                printf("%c",k);
             }
             else{
                 printf(" ");
